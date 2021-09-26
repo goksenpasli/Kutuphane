b13110fd8165332cf4f1d60d3b645508c4b9727f
goksenpasli <gpasli@gmail.com>
2021-09-26 18:19:40 +0300

    Fix Code

--- Kutuphane/ViewModel/KitapGirişViewModel.cs
+++ Kutuphane/ViewModel/KitapGirişViewModel.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Kutuphane.ViewModel
@@ -43,6 +44,7 @@ namespace Kutuphane.ViewModel
                 }
 
                 MainViewModel.DatabaseSave.Execute(null);
+
                 ResetKitap();
                 if (Kitap.OtomatikBarkod)
                 {
@@ -50,23 +52,21 @@ namespace Kutuphane.ViewModel
                 }
             }, parameter => Kitap.DolapId != 0 && !string.IsNullOrWhiteSpace(Kitap?.Ad));
 
-            KitapTürü tür = null;
             KitapTürEkle = new RelayCommand<object>(parameter =>
             {
                 if (parameter is Kütüphane kütüphane)
                 {
-                    tür = new KitapTürü() { Açıklama = Kitap.Tür, Id = new Random(Guid.NewGuid().GetHashCode()).Next(1, int.MaxValue) };
+                    var tür = new KitapTürü() { Açıklama = Kitap.Tür, Id = new Random(Guid.NewGuid().GetHashCode()).Next(1, int.MaxValue) };
                     kütüphane?.KitapTürleri.Add(tür);
                     MainViewModel.DatabaseSave.Execute(null);
                 }
             }, parameter => parameter is Kütüphane kütüphane && !kütüphane.KitapTürleri.Any(z => z.Açıklama == Kitap.Açıklama) && !string.IsNullOrWhiteSpace(Kitap?.Tür));
 
-            Yazar yazar = null;
             KitapYazarEkle = new RelayCommand<object>(parameter =>
             {
                 if (parameter is Kütüphane kütüphane)
                 {
-                    yazar = new Yazar() { Id = new Random(Guid.NewGuid().GetHashCode()).Next(1, int.MaxValue), Ad = Kitap.Yazar };
+                    var yazar = new Yazar() { Id = new Random(Guid.NewGuid().GetHashCode()).Next(1, int.MaxValue), Ad = Kitap.Yazar };
                     kütüphane?.Yazarlar.Add(yazar);
                     MainViewModel.DatabaseSave.Execute(null);
                 }
@@ -88,22 +88,29 @@ namespace Kutuphane.ViewModel
                 OpenFileDialog openFileDialog = new() { Multiselect = false, Filter = "Excel Dosyaları (*.xls;*.xlsx)|*.xls;*.xlsx" };
                 if (openFileDialog.ShowDialog() == true)
                 {
-                    using var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-                    using var reader = ExcelReaderFactory.CreateReader(stream);
-                    do
+                    try
                     {
-                        while (reader.Read())
+                        using var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                        using var reader = ExcelReaderFactory.CreateReader(stream);
+                        do
                         {
-                            (parameter as Kütüphane)?.Kitaplar.Add(new Kitap
+                            while (reader.Read())
                             {
-                                Id = new Random(Guid.NewGuid().GetHashCode()).Next(1, int.MaxValue),
-                                Ad = reader.GetValue(0).ToString(),
-                                Barkod = reader.GetValue(1).ToString(),
-                                DolapId = Kitap.DolapId
-                            });
-                        }
-                    } while (reader.NextResult());
-                    MainViewModel.DatabaseSave.Execute(null);
+                                (parameter as Kütüphane)?.Kitaplar.Add(new Kitap
+                                {
+                                    Id = new Random(Guid.NewGuid().GetHashCode()).Next(1, int.MaxValue),
+                                    Ad = reader.GetValue(0).ToString(),
+                                    Barkod = reader.GetValue(1).ToString(),
+                                    DolapId = Kitap.DolapId
+                                });
+                            }
+                        } while (reader.NextResult());
+                        MainViewModel.DatabaseSave.Execute(null);
+                    }
+                    catch (Exception ex)
+                    {
+                        _ = MessageBox.Show(ex.Message, "KÜTÜPHANE", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    }
                 }
             }, parameter => Kitap.DolapId != 0);
 
@@ -164,7 +171,7 @@ namespace Kutuphane.ViewModel
 
         private Kitap KitapOluştur()
         {
-            return new Kitap
+            var kitap = new Kitap
             {
                 Id = new Random(Guid.NewGuid().GetHashCode()).Next(1, int.MaxValue),
                 Ad = Kitap.Ad,
@@ -178,12 +185,19 @@ namespace Kutuphane.ViewModel
                 Fiyat = Math.Round(Kitap.Fiyat, 2),
                 KitapDurumId = Kitap.KitapDurumId,
                 SistemKayıtTarihi = DateTime.Now,
-                Yazarlar = SeçiliYazarlar,
-                Türler = SeçiliKitapTürleri,
                 Resim = Kitap.Resim,
                 ÖdünçVerilebilir = Kitap.ÖdünçVerilebilir,
                 KitapDili = Kitap.KitapDili
             };
+            foreach (var item in SeçiliYazarlar)
+            {
+                kitap.Yazarlar.Add(item);
+            }
+            foreach (var item in SeçiliKitapTürleri)
+            {
+                kitap.Türler.Add(item);
+            }
+            return kitap;
         }
 
         private void ResetKitap()
