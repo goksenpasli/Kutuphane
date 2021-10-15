3f3e1dd4b1aba8a02c76a940c142a726e132d4c6
goksenpasli <gpasli@gmail.com>
2021-10-15 20:18:27 +0300

    Added Some Property

--- Kutuphane/ViewModel/KitapTakvimViewModel.cs
+++ Kutuphane/ViewModel/KitapTakvimViewModel.cs
@@ -4,9 +4,36 @@ using Kutuphane.View;
 using System;
 using System.ComponentModel;
 using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
 namespace Kutuphane.ViewModel
 {
+    public static class CalandarHelper
+    {
+        public static readonly DependencyProperty SingleClickDefocusProperty = DependencyProperty.RegisterAttached("SingleClickDefocus", typeof(bool), typeof(CalandarHelper), new FrameworkPropertyMetadata(false, new PropertyChangedCallback(SingleClickDefocusChanged)));
+
+        public static bool GetSingleClickDefocus(DependencyObject obj) => (bool)obj.GetValue(SingleClickDefocusProperty);
+
+        public static void SetSingleClickDefocus(DependencyObject obj, bool value) => obj.SetValue(SingleClickDefocusProperty, value);
+
+        private static void SingleClickDefocusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Calendar calendar)
+            {
+                calendar.PreviewMouseDown += (a, b) =>
+                {
+                    if (Mouse.Captured is Calendar or CalendarItem)
+                    {
+                        _ = Mouse.Capture(null);
+                    }
+                };
+            }
+        }
+    }
+
     public class KitapTakvimViewModel : InpcBase
     {
         private DateTime? seçiliGün;
